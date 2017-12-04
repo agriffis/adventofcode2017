ccf43b2ffb384a9518f27b51d19b674b8f6040ed
Aron Griffis <aron@scampersand.com>
2017-12-04 09:13:35 -0500

    day 1

--- test/adventofcode2017/core_test.clj
+++ test/adventofcode2017/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [adventofcode2017.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-day-1
+  (testing "Day 1. Inverse Captcha"
+    (is (= (day-1a) 1223))
+    (is (= (day-1b) 1284))))
