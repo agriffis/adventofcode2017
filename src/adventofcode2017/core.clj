4b18bffd46d08aacd2db0add792faefc3dd8a069
Aron Griffis <aron@scampersand.com>
2017-12-07 22:42:51 -0500

    day 6

--- src/adventofcode2017/core.clj
+++ src/adventofcode2017/core.clj
@@ -2,6 +2,11 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
 (defn parse-int
   [s]
   (. Integer parseInt (str s)))
@@ -751,7 +756,72 @@ xzvfbf fopmfxu mvftgr mfupoxf coyhof talcc vpkslo")
   ([maze] (day-5b 0 maze 0))
   ([] (day-5b day-5-input)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;======================================================================
+;; Day 6. Memory Reallocation
+;;======================================================================
+
+(def day-6-input [11 11 13 7 0 15 5 5 4 4 1 1 7 1 15 11])
+
+(defn bank-with-most
+  [banks]
+  ;; min-key and max-key return the last index in case of a tie.
+  ;; We want the first, so rseq and math.
+  (let [ri (first (apply max-key second (map-indexed vector (rseq banks))))]
+    (- (count banks) ri 1)))
+
+(defn rotate-banks
+  "Rotate banks so index is at the end."
+  [banks index]
+  (let [banksv (vec banks)]
+    (vec (concat (subvec banksv (inc index))
+                 (subvec banksv 0 (inc index))))))
+
+(defn unrotate-banks
+  "Undo rotate-banks."
+  [banks index]
+  (rotate-banks banks (- (count banks) index 2)))
+
+(defn redistribute-bank
+  ([banks index]
+   (let [value (get banks index)
+         n (count banks)]
+     (-> banks
+         (assoc index 0)
+         (rotate-banks index)
+         (#(map-indexed (fn [i v] (+ v (quot value n)
+                                     (if (< i (mod value n)) 1 0))) %))
+         (unrotate-banks index))))
+  ([banks]
+   (redistribute-bank banks (bank-with-most banks))))
+
+(defn -while-unique
+  ([s]
+   (-while-unique s #{}))
+  ([s seen]
+   (when (seq s)
+     (let [v (first s) vs (rest s)]
+       (when-not (contains? seen v)
+         (lazy-seq (cons [v vs] (-while-unique vs (conj seen v)))))))))
+
+(defn take-while-unique
+  [s]
+  (map first (-while-unique s)))
+
+(defn drop-while-unique
+  [s]
+  (second (last (-while-unique s))))
+
+(defn day-6a
+  "Day 6. Memory Reallocation - Part 1"
+  ([input]
+   (count (take-while-unique (iterate redistribute-bank input))))
+  ([] (day-6a day-6-input)))
+
+(defn day-6b
+  "Day 6. Memory Reallocation - Part 2"
+  ([input]
+   (let [wu (-while-unique (iterate redistribute-bank input))
+         uniques (mapv first wu)
+         repeated (first (second (last wu)))]
+     (count (drop-while #(not= % repeated) uniques))))
+  ([] (day-6b day-6-input)))
