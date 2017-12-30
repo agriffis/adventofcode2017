f29103c87f9e378e8d85a1d43575c64cd44471d8
Aron Griffis <aron@scampersand.com>
2017-12-29 21:29:31 -0500

    day 16

---
+++ src/adventofcode2017/day16.clj
@@ -0,0 +1,48 @@
+(ns adventofcode2017.day16
+  (:require [clojure.string :as str]))
+
+(def dispatch {\s (fn [line x]
+                    (let [i (- (count line) (Integer/parseInt x))]
+                      (str/join
+                        (concat (drop i line) (take i line)))))
+               \x (fn [line x]
+                    (let [[a b] (map #(Integer/parseInt %) (str/split x #"/"))]
+                      (str/join
+                        (assoc (vec line)
+                               a (get line b)
+                               b (get line a)))))
+               \p (fn [line x]
+                    (let [[a b] (map #(str/index-of line %) (str/split x #"/"))]
+                      (str/join
+                        (assoc (vec line)
+                               a (get line b)
+                               b (get line a)))))})
+
+(defn- run-program
+  [line input]
+  (loop [line line
+         program (str/split input #",")]
+    (let [ins (first program)]
+      (if-not ins
+        line
+        (recur ((dispatch (first ins)) line (subs ins 1))
+               (rest program))))))
+
+(def input (slurp "resources/day16.txt"))
+
+(defn day-16a [] (run-program "abcdefghijklmnop" input))  ; "kgdchlfniambejop"
+
+(defn- find-cycle-length
+  []
+  (loop [n 0
+         line "abcdefghijklmnop"
+         results #{}]
+    (if (contains? results line)
+      n
+      (recur (inc n)
+             (run-program line input)
+             (conj results line)))))  ; 42
+
+(defn day-16b []
+  (nth (iterate #(run-program % input) "abcdefghijklmnop")
+       (mod 1000000000 42)))  ; "fjpmholcibdgeakn"
