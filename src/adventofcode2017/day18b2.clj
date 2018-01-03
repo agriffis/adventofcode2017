9cf057d179830bf6981d7a604bee5ed302d7aaf5
Aron Griffis <aron@scampersand.com>
2018-01-03 08:13:26 -0500

    doc strings

--- src/adventofcode2017/day18b2.clj
+++ src/adventofcode2017/day18b2.clj
@@ -4,6 +4,9 @@
 (def input (slurp "resources/day18.txt"))
 
 (defn dispatch
+  "Execute a single instruction. Returns the updated registers. Will block
+  on rcv. Adds {:done true} if last thread to block, or if sender doesn't
+  supply a new promise."
   [regs ins]
   (let [[i a b] (re-seq #"\S+" ins)
         [va vb] (map #(try (Integer/parseInt %)
@@ -23,18 +26,27 @@
              "rcv" (if (= cnt (swap! blk inc))
                      {:done true}
                      (let [[v p] @rcv]
-                       (if v {a v :rcv p} {:done true})))))))
+                       (if p {a v :rcv p} {:done true})))))))
 
 (defn run-program
+  "Run single instance of program on registers. Returns a lazy sequence of
+  program states. Stops when either regs contains {:done true} or run out
+  of instructions."
   [{:keys [ip done snd] :as regs} program]
   (let [ins (nth program ip nil)]
     (if (or done (not ins))
-      (do (deliver snd nil) nil)
+      (do
+        (deliver snd nil)  ; Unblock other thread to finish.
+        nil)               ; End of program states.
       (lazy-seq
        (let [next-regs (dispatch regs ins)]
          (cons next-regs (run-program next-regs program)))))))
 
 (defn run-futures
+  "Run multiple instances of program as futures. funs should be a list of
+  functions to process the sequence of program states; the length of funs
+  determines the number of futures. Processing happens in the future before
+  returning, to avoid holding the seq head unless needed by the caller."
   [input funs]
   (let [program (vec (str/split input #"\n"))
         mailboxes (repeatedly promise)
@@ -52,9 +64,8 @@
      funs)))
 
 (defn day-18b
-  ([] (day-18b input))
-  ([input]
-   (let [funs [dorun
-               #(dec (count (dedupe (map :snd %))))]]
-     (map deref (run-futures input funs)))))
+  [input]
+  (let [funs [dorun  ; ignore program 0
+              #(dec (count (dedupe (map :snd %))))]]
+    (map deref (run-futures input funs))))
 ;=> (nil 7239)
