6f21a7479979a90cfe0195e60ca12370fa40cf74
Aron Griffis <aron@scampersand.com>
2018-01-10 21:35:10 -0500

    loop/recur for day 5

--- src/adventofcode2017/day05.clj
+++ src/adventofcode2017/day05.clj
@@ -1,28 +1,29 @@
 (ns adventofcode2017.day05
-  (:use adventofcode2017.inputs))
+  (:require [clojure.string :as str]))
 
-;;======================================================================
-;; Day 5. A Maze of Twisty Trampolines, All Alike
-;;======================================================================
+(def input
+  (->> (slurp "resources/day05.txt")
+       str/split-lines
+       (map #(Integer/parseInt %))))
 
-(defn day-5a
-  "Day 5. A Maze of Twisty Trampolines, All Alike - Part 1"
-  ([jumps maze pos]
-   (let [j (get maze pos)]
-     (if-not j
-       jumps
-       (recur (inc jumps) (assoc maze pos (inc j)) (+ pos j)))))
-  ([maze] (day-5a 0 maze 0))
-  ([] (day-5a day-5-input)))
+(defn part1
+  [input]
+  (loop [jumps 0
+         maze (vec input)
+         pos 0]
+    (let [j (get maze pos)]
+      (if-not j
+        jumps
+        (recur (inc jumps) (update maze pos inc) (+ pos j))))))
 
-(defn day-5b
-  "Day 5. A Maze of Twisty Trampolines, All Alike - Part 2"
-  ([jumps maze pos]
-   (let [j (get maze pos)]
-     (if-not j
-       jumps
-       (recur (inc jumps)
-              (assoc maze pos (if (> j 2) (dec j) (inc j)))
-              (+ pos j)))))
-  ([maze] (day-5b 0 maze 0))
-  ([] (day-5b day-5-input)))
+(defn part2
+  [input]
+  (loop [jumps 0
+         maze (vec input)
+         pos 0]
+    (let [j (get maze pos)]
+      (if-not j
+        jumps
+        (recur (inc jumps)
+               (assoc maze pos (if (> j 2) (dec j) (inc j)))
+               (+ pos j))))))
